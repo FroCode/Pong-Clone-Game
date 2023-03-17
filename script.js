aac20bbcd19b9ecf877ff015edca20a2fdf04ef2
HADDOUCHE MAAMAR <frocodeof@gmail.com>
2023-03-18 00:48:59 +0100

    Up-2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log()
+// Update Loop
+import Ball from './Ball.js'
+const ball = new Ball(document.getElementById("ball"))
+function update(time) {
+
+}
+setInterval(update, 10)
+window.requestAnimationFrame(update)
