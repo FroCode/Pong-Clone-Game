1bcbe00d8c66c2d8f2aa4f16d94cf604bd4cace8
HADDOUCHE MAAMAR <frocodeof@gmail.com>
2023-03-18 01:45:03 +0100

    3

--- Ball.js
+++ Ball.js
@@ -3,12 +3,19 @@ export default class Ball {
         this.ballElem = ballElem
     }
     get x() {
-        return getComputedStyle(this.ballElem).getPropertyValue("--x")
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
     }
     set x(value) {
         this.ballElem(this.ballElem).setProperty("--x", value)
     }
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+    set y(value) {
+        this.ballElem(this.ballElem).setProperty("--y", value)
+    }
     update(delta) {
         this.x = 5
+        this.y = 15
     }
 }
