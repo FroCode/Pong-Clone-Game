0982715d70ec8da2698f2fc9d87767188beccdf8
HADDOUCHE MAAMAR <frocodeof@gmail.com>
2023-03-18 01:31:36 +0100

    3

--- Ball.js
+++ Ball.js
@@ -2,5 +2,13 @@ export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
     }
-
+    get x() {
+        return getComputedStyle(this.ballElem).getPropertyValue("--x")
+    }
+    set x(value) {
+        this.ballElem(this.ballElem).setProperty("--x", value)
+    }
+    update(delta) {
+        this.x = 5
+    }
 }
--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // Update Loop
 import Ball from './Ball.js'
 const ball = new Ball(document.getElementById("ball"))
+let last_time
 function update(time) {
+    if (last_time != null ) {
+        const delta = time - last_time
+        // Update Code
+        ball.update(delta)
 
+    }
+    last_time = time
+    window.requestAnimationFrame(update)
 }
-setInterval(update, 10)
+
 window.requestAnimationFrame(update)
