556ce67fb4a918cbeef3d2d0480a7dfc923d57a9
HADDOUCHE MAAMAR <frocodeof@gmail.com>
2023-03-18 22:46:24 +0100

    3

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-// Update Loop
-import Ball from './Ball.js'
-const ball = new Ball(document.getElementById("ball"))
-let last_time
-function update(time) {
-    if (last_time != null ) {
-        const delta = time - last_time
-        // Update Code
-        ball.update(delta)
+import Ball from "./Ball.js"
+import Paddle from "./Padlle.js"
 
+const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
+
+let lastTime
+function update(time) {
+    if (lastTime != null) {
+        const delta = time - lastTime
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+        computerPaddle.update(delta, ball.y)
+        const hue = parseFloat(
+            getComputedStyle(document.documentElement).getPropertyValue("--hue")
+        )
+
+        document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
+
+        if (isLose()) handleLose()
     }
-    last_time = time
+
+    lastTime = time
     window.requestAnimationFrame(update)
 }
+
+function isLose() {
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose() {
+    const rect = ball.rect()
+    if (rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
 
 window.requestAnimationFrame(update)
